af4ca4f0e7fb2679d9421b71d8c6a57423f5d666
Carlo Dapor <carlo.dapor@mimacom.com>
2024-01-28 22:57:32 +0100

    Check for wget, wget2, curl; check for lhasa and lha.

--- install_amiga_toolchain.sh
+++ install_amiga_toolchain.sh
@@ -1,65 +1,96 @@
 #!/bin/sh
 
-if ! command -v wget &> /dev/null
+if [ -z ${VBCC} ]
 then
-    echo "Please install wget !"
-    exit
+  echo "Environment variable VBCC not set; please define it."
+  exit 1
 fi
 
-if ! command -v lha &> /dev/null
+echo "Environment variable VBCC points to ${VBCC}."
+
+mkdir -p $VBCC
+
+command -v curl &> /dev/null && fetch=curl
+command -v wget &> /dev/null && fetch=wget
+command -v wget2 &> /dev/null && fetch=wget2
+
+if [ -z "${fetch}" ]
 then
-    echo "Please install an lha decompressor !"
-    exit
+  echo "Could not find wget, wget2 nor curl; please install either one."
+  exit 1
 fi
 
-mkdir vbcc_tools
-mkdir vbcc
+echo "Command to fetch is ${fetch}."
+
+command -v lhasa &> /dev/null && lha=lhasa
+command -v lha &> /dev/null && lha=lha
+
+if [ -z "${lha}" ]
+then
+  echo "Could not find lha nor lhasa; please install either one."
+  exit 1
+fi
+
+echo "Command to unpack .lha is ${lha}."
+
+mkdir -p vbcc_tools
+mkdir -p vbcc
+
 cd vbcc_tools
 
-wget http://sun.hasenbraten.de/vasm/release/vasm.tar.gz
-wget http://sun.hasenbraten.de/vlink/release/vlink.tar.gz
-wget http://www.ibaug.de/vbcc/vbcc.tar.gz
-wget http://phoenix.owl.de/vbcc/current/vbcc_target_m68k-amigaos.lha
-wget http://phoenix.owl.de/vbcc/current/vbcc_unix_config.tar.gz
+${fetch} http://sun.hasenbraten.de/vasm/release/vasm.tar.gz
+${fetch} http://sun.hasenbraten.de/vlink/release/vlink.tar.gz
+${fetch} http://www.ibaug.de/vbcc/vbcc.tar.gz
+${fetch} http://phoenix.owl.de/vbcc/current/vbcc_target_m68k-amigaos.lha
+${fetch} http://phoenix.owl.de/vbcc/current/vbcc_unix_config.tar.gz
 
-tar zxvf vbcc.tar.gz
+tar -zxpf vbcc.tar.gz
 cd vbcc
-if [ ! -d bin ]; then
-    mkdir bin
-fi
-make TARGET=m68k
-cp -r bin ../../vbcc/
+mkdir -p bin
+ln -s ucpp vcpp
+TARGET=m68k
+make CC='gcc -std=c9x -g' TARGET=${TARGET} bin/dtgen bin/vc bin/vprof bin/vbcc${TARGET}
+cp -prf bin ../../vbcc
+TARGET=m68ks
+make CC='gcc -std=c9x -g' TARGET=${TARGET} bin/vbcc${TARGET}
+cp -prf bin ../../vbcc
 
 cd ..
-lha x vbcc_target_m68k-amigaos.lha
-cp -r vbcc_target_m68k-amigaos/* ../vbcc/
+${lha} -x -qf vbcc_target_m68k-amigaos.lha
+cp -pr vbcc_target_m68k-amigaos/* ../vbcc
 
 cd ../vbcc
-tar zxvf ../vbcc_tools/vbcc_unix_config.tar.gz
-
-export VBCC=~/amiga-cc/vbcc/
-export PATH=$VBCC/bin:$PATH
+tar -zxpf ../vbcc_tools/vbcc_unix_config.tar.gz
 
 cd ../vbcc_tools
-tar zxvf vasm.tar.gz
+tar -zxpf vasm.tar.gz
 cd vasm
-make CPU=m68k SYNTAX=mot
-cp vasmm68k_mot vobjdump $VBCC/bin
+make CPU=m68k SYNTAX=mot ; make clean
+make CPU=m68k SYNTAX=oldstyle ; make clean
+make CPU=m68k SYNTAX=std
+cp -pf vasmm68k_mot vasmm68k_oldstyle vasmm68k_std vobjdump ../../vbcc/bin
 
 cd ..
-tar zxvf vlink.tar.gz
+tar -zxpf vlink.tar.gz
 cd vlink
 make
-cp vlink $VBCC/bin
+cp -pf vlink ../../vbcc/bin
 
-cd $VBCC
-if [ ! -d NDK_3.2 ]; then
-    mkdir NDK_3.2
-fi
+cd ../../vbcc
+mkdir -p NDK_3.2
+
 cd NDK_3.2
-wget http://aminet.net/dev/misc/NDK3.2.lha
-lha x NDK3.2.lha
+${fetch} https://aminet.net/dev/misc/NDK3.2.lha
+${lha} -x -qf NDK3.2.lha
 cd ..
-cp -r $VBCC/NDK_3.2/Include_h/* $VBCC/targets/m68k-amigaos/include
-rm -rf $VBCC/NDK_3.2/
-rm -rf ~/amiga-cc/vbcc_tools/
+cp -prf NDK_3.2/Include_h/* targets/m68k-amigaos/include
+rm -rf NDK_3.2
+
+cd ..
+
+rm -rf vbcc/Install* vbcc/bin/.dummy vbcc/bin/*.dSYM
+chmod 644 vbcc/config/*
+
+cp -pfr vbcc/* $VBCC/
+
+unset TARGET fetch lha
